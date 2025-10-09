f7b9cd47fb072fcaa88505208a0d849c72e37107
nngtkhngoc <khanhngoc230705@gmail.com>
2025-10-10 00:51:57 +0700

    feat: add HowItWorks component

--- src/pages/User/HomePage/components/HeroSection.tsx
+++ src/pages/User/HomePage/components/HeroSection.tsx
@@ -5,18 +5,18 @@ import { PrimaryButton } from "../../../../components/ui/User/Button";
 
 export const HeroSection = () => {
   return (
-    <div className="bg-gradient-to-b from-[#F1EBFF] to-[#EDE5FF]">
-      <div className="px-6 pt-[100px] pb-[50px]  md:flex md:flex-row md:px-20 md:justify-around md:items-center">
+    <section className="bg-gradient-to-b from-[#F1EBFF] to-[#EDE5FF]">
+      <div className="px-6 pt-[100px] pb-[50px]  lg:flex lg:flex-row lg:px-20 lg:justify-around lg:items-center">
         <img
           src={illustration}
           alt="finding jobs"
-          className="block md:hidden"
+          className="block lg:hidden"
         />
-        <div className="flex flex-col gap-3 md:items-start md:max-w-[670px] md:gap-5">
-          <div className="hidden md:block bg-gradient-to-r from-[#7E4BEB] via-[#4B18B8] to-[#38128A] text-secondary font-bold font-[16px] px-[20px] py-[4px] rounded-[30px]">
+        <section className="flex flex-col gap-3 lg:items-start lg:max-w-[670px] lg:gap-5">
+          <div className="hidden lg:block bg-gradient-to-r from-[#7E4BEB] via-[#4B18B8] to-[#38128A] text-secondary font-bold font-[16px] px-[20px] py-[4px] rounded-[30px]">
             HireHub
           </div>
-          <div className="text-center text-[36px] font-extrabold font-title leading-[40px] md:text-[80px] md:text-left md:leading-[80px]">
+          <h3 className="text-center text-[36px] font-extrabold font-title leading-[40px] lg:text-[80px] lg:text-left lg:leading-[80px]">
             <div className="">
               <span className="text-primary">TÌM VIỆC </span>
               <span className="text-[#8DB82D]">DỄ, </span>
@@ -25,37 +25,37 @@ export const HeroSection = () => {
               <span className="text-primary">CHỌN NGHỀ </span>
               <span className="text-[#8DB82D]">HAY</span>
             </div>
-          </div>
-          <div className="text-[12px] text-center leading-[26px] px-10 text-[#263238] block md:hidden">
+          </h3>
+          <div className="text-[12px] text-center leading-[26px] px-5 text-[#263238] block lg:hidden">
             Khám phá hàng trăm cơ hội việc làm, kết nối trực tiếp với nhà tuyển
             dụng và tìm công việc thật sự phù hợp với kỹ năng, đam mê của bạn.
           </div>
-          <div className="text-[14px] text-left leading-[26px] text-[#263238] md:block hidden">
+          <div className="text-[14px] text-left leading-[26px] text-[#263238] lg:block hidden">
             Ở đây, bạn có thể dễ dàng khám phá hàng trăm cơ hội nghề nghiệp, kết
             nối trực tiếp với nhà tuyển dụng, và quan trọng nhất là tìm được
             công việc thực sự phù hợp với kỹ năng, đam mê và phong cách sống của
             riêng mình.
           </div>
           <div className="flex flex-row justify-around items-center gap-[12px]">
-            <div className="flex flex-row justify-around items-center bg-white rounded-[30px] w-[215px] h-[40px] shadow-[0_4px_4px_0_#DFD2FA] md:w-[270px]">
-              <div className="text-[12px] md:text-[14px] text-[#7A7D87] md:flex md:flex-row md:items-center ">
-                Tìm kiếm việc làm<span className="hidden md:block">...</span>
+            <div className="flex flex-row justify-around items-center bg-white rounded-[30px] w-[160px] h-[40px] shadow-[0_4px_4px_0_#DFD2FA] lg:w-[270px]">
+              <div className="text-[12px] lg:text-[14px] text-[#7A7D87] lg:flex lg:flex-row lg:items-center ">
+                Tìm kiếm việc làm<span className="hidden lg:block">...</span>
               </div>
               <Search size={18} className="hover:cursor-pointer" />
             </div>
             <PrimaryButton
               label="Tìm kiếm"
               paddingY="py-[11px]"
-              textSize="text-[12px] md:text-[14px]"
+              textSize="text-[12px] lg:text-[14px]"
             />
           </div>
-        </div>
+        </section>
         <img
           src={illustration}
           alt="finding jobs"
-          className="md:block hidden"
+          className="lg:block hidden "
         />
       </div>
-    </div>
+    </section>
   );
 };
--- src/pages/User/HomePage/components/HowItWorks.tsx
+++ src/pages/User/HomePage/components/HowItWorks.tsx
@@ -1,3 +1,73 @@
+import { NumberCircle } from "./NumberCircle";
+
+import howitworks from "../../../../assets/illustration/howitworks.png";
+
+interface Step {
+  title: string;
+  description: string;
+}
+
 export const HowItWorks = () => {
-  return <div className="">HowItWorks</div>;
+  const steps: Step[] = [
+    {
+      title: "Tạo tài khoản",
+      description:
+        "Tạo hồ sơ cá nhân nhanh chóng bằng email hoặc tạo tài khoản mới.",
+    },
+    {
+      title: "Hoàn thiện hồ sơ & CV",
+      description: "Điền thông tin chi tiết về học vấn, kinh nghiệm, kỹ năng.",
+    },
+    {
+      title: "Tìm kiếm & Ứng tuyển việc làm",
+      description: "Khám phá hàng trăm công việc từ nhiều công ty.",
+    },
+    {
+      title: "Tạo tài khoản",
+      description:
+        "Theo dõi trạng thái ứng tuyển, nhận thông báo khi nhà tuyển dụng xem hồ sơ và phản hồi.",
+    },
+  ];
+
+  const renderSteps = () => {
+    return steps.map((step, index) => {
+      const number = "0" + (index + 1);
+      return (
+        <div className="flex flex-row items-start gap-3">
+          <NumberCircle number={number} />
+          <div className="flex flex-col gap-1 justify-start max-w-[275px]">
+            <div className="text-[#8DB82D] font-bold text-[17px]">
+              {step.title}
+            </div>
+            <div className="text-[#A6A6A6] text-[12px] leading-[22px]">
+              {step.description}
+            </div>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <section className="py-10 lg:flex lg:flex-row lg:justify-center lg:gap-20 flex-col items-center justify-center lg:px-10">
+      <img src={howitworks} alt="computer" className="lg:block hidden" />
+
+      <section>
+        <section className="flex flex-col items-center lg:items-start">
+          <div className="w-fit text-center">
+            <h3 className="relative text-center text-primary text-[30px] font-extrabold  font-title lg:text-[48px] lg:text-left  whitespace-nowrap after:content-[''] after:absolute after:left-0 after:bottom-1 lg:after:bottom-3 after:w-full after:h-[13px] sm:after:h-[20px] after:bg-secondary after:-z-10 ">
+              QUY TRÌNH ĐƠN GIẢN
+            </h3>
+          </div>
+
+          <div className="flex flex-col gap-5 px-2 py-8 lg:px-0 lg:justify-start lg:items-start ">
+            {renderSteps()}
+          </div>
+        </section>
+      </section>
+      <div className="w-full flex items-center justify-center block lg:hidden">
+        <img src={howitworks} alt="computer" />
+      </div>
+    </section>
+  );
 };
