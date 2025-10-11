d39a44af7a219b575902db721304e976eb9ec11b
nngtkhngoc <khanhngoc230705@gmail.com>
2025-10-11 15:06:22 +0700

    feat: add ForgetPasswordPage

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,8 @@ import { UserLayout } from "./components/layout/User/UserLayout.tsx";
 import { HomePage } from "./pages/User/HomePage/HomePage.tsx";
 import { AuthPage } from "./pages/User/Auth/AuthPage.tsx";
 import { AuthLayout } from "./components/layout/User/AuthLayout.tsx";
-import { VerifyPage } from "./pages/User/Verify/components/VerifyPage.tsx";
+import { VerifyPage } from "./pages/User/Verify/VerifyPage.tsx";
+import { ForgetPasswordPage } from "./pages/User/ForgetPassword/ForgetPasswordPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -20,6 +21,10 @@ createRoot(document.getElementById("root")!).render(
         <Route path="/auth" element={<AuthLayout />}>
           <Route index element={<AuthPage />} />
           <Route path="/auth/verify" element={<VerifyPage />} />
+          <Route
+            path="/auth/forget-password"
+            element={<ForgetPasswordPage />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
