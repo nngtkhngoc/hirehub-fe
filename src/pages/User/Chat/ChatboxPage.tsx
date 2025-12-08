2951bef84ba510129a73b19e9afa9a0d3b8b3dc8
nngtkhngoc <khanhngoc230705@gmail.com>
2025-12-09 00:06:17 +0700

    chore: chatbox layout

--- src/pages/User/Chat/ChatboxPage.tsx
+++ src/pages/User/Chat/ChatboxPage.tsx
@@ -4,19 +4,22 @@ import { UserDetail } from "./components/layout/UserDetail";
 import { ChatList } from "./components/layout/ChatList";
 import { useUserById } from "@/hooks/useUser";
 import { ChevronLeft } from "lucide-react";
+import { Header } from "@/components/layout/User/Header";
 
 export const ChatboxPage = () => {
   const { id } = useParams();
   const { data: receiver, isLoading } = useUserById(parseInt(id!));
 
   return (
-    <div className="bg-white w-full h-screen ">
-      {" "}
-      <div className=" px-4 bg-[#F2F4F7] pb-[50px] h-full  w-full flex items-center justify-center flex-col">
-        <div className="w-full flex items-center px-10 justify-left gap-1 text-md  hover:cursor-pointer hover:text-primary transition-all duration-300 hover:font-bold">
+    <div className="bg-white w-full h-screen flex flex-col">
+      <div>
+        <Header />
+      </div>
+      <div className="px-4  bg-[#F2F4F7]  w-full flex-1 flex items-center justify-center flex-col">
+        {/* <div className="w-full flex items-center px-10 justify-left gap-1 text-md  hover:cursor-pointer hover:text-primary transition-all duration-300 hover:font-bold">
           <ChevronLeft />
           Back
-        </div>
+        </div> */}
         <div className="flex flex-row gap-8 py-5 px-8 w-full">
           <div className="w-1/4">
             <ChatList />
