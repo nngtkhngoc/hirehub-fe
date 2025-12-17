29babdf08128a404b66c7fdbc7e8b0ed46db3047
uqtri <ungquangtri@gmail.com>
2025-12-17 22:49:32 +0700

    Update api in chat.api.ts

--- src/apis/chat.api.ts
+++ src/apis/chat.api.ts
@@ -2,23 +2,14 @@ import { axiosClient, axiosClientFormData } from "@/lib/axios";
 
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
-export const getHistory = async (getHistoryParams: {
-  userA: number;
-  userB: number;
+export const getHistory = async (params: {
+  conversationId: number;
+  userId: number;
+  messageTypes?: string[];
 }) => {
   const res = await axiosClient.get(`${BASE_URL}/api/messages/history`, {
-    params: getHistoryParams,
+    params,
   });
-  return res.data;
-};
-
-export const getChatList = async (getChatListParams: {
-  userId: number | null;
-}) => {
-  const res = await axiosClient.get(`${BASE_URL}/api/messages/chat-list`, {
-    params: getChatListParams,
-  });
-
   return res.data;
 };
 
